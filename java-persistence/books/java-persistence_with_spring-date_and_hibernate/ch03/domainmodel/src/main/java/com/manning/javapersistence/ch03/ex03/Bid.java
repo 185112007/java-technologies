781e00984c28ea09a3b9ebce35b529090db825ef
gafur hayytbayev <gafur.hayytbayev@gmail.com>
2025-01-12 14:56:38 +0300

    add domainmodel, validation examples

---
+++ java-persistence/books/java-persistence_with_spring-date_and_hibernate/ch03/domainmodel/src/main/java/com/manning/javapersistence/ch03/ex03/Bid.java
@@ -0,0 +1,13 @@
+package com.manning.javapersistence.ch03.ex03;
+
+public class Bid {
+    private Item item;
+
+    public Item getItem(){
+        return item;
+    }
+
+    void setItem(Item item) {
+        this.item = item;
+    }
+}
---
+++ java-persistence/books/java-persistence_with_spring-date_and_hibernate/ch03/validation/src/main/java/com/manning/javapersistence/ch03/validation/Item.java
@@ -0,0 +1,35 @@
+package com.manning.javapersistence.ch03.validation;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Item {
+    @NotNull
+    @Size(
+        min = 2,
+        max = 255,
+        message = "Name is required, maximum 255 characters."
+    )
+    private String name;
+    @Future
+    private Date auctionEnd;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getAuctionEnd(){
+        return auctionEnd;
+    }
+
+    public void setAuctionEnd(Date auctionEnd) {
+        this.auctionEnd = auctionEnd;
+    }
+}
